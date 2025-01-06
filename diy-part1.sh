9d27c5336981e5cd35582172bc45699326676296
sfwtw <wtw@cr.cx>
2025-01-06 22:54:48 +0800

    更新工作流，添加对 iStore 的支持并更新 feeds 配置

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,3 +17,4 @@
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git modem https://github.com/FUjr/modem_feeds.git;main' >>feeds.conf.default
+echo 'src-git CONFIG_FEED_modem=y https://github.com/linkease/istore;main' >> feeds.conf.default
